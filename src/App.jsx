9286593d661c4df90c4dbb3c25b9eaaa9b5e0acb
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-01-13 15:26:33 -0500

    finished the basic function of the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,65 @@ import Quiz from "./components/Quiz"
 import { decode } from 'html-entities'
 
 export default function App() {
+  // setting temporary score 
+  const tempScore =[]
+
+  for (let i= 0; i < 5; i++)  {
+    tempScore.push({
+      isCorrect: false,
+      answer: "temp",
+      index: i})
+  }
+
+  // States
   const[question, setQuestion] = useState([])
-  const [score, setScore] = useState([false, false, false, false, false])
+  const [score, setScore] = useState(tempScore)
   const [checkScore, setCheckScore] = useState(()=> temp)
   const [isStartQuiz, setStartQuiz] = useState(false)
+  const [showAnswer, setShowAnswer] = useState(false)
+  const [totalScore, setTotalScore] = useState(0)
+  const [apiError, setApiError] = useState(null);
 
   function temp(){
-    console.log("happy new year")
+    alert("You have not started the test")
   }
 
   useEffect(()=>{
     if(isStartQuiz) {
-    
-      fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
-        .then(res => res.json())
-        .then(data => {
-          let results = data.results
-
-          let questions = results.map(result => {
-            let answer = result.incorrect_answers
-            let correctAnswer = result.correct_answer
-            let random = Math.ceil(Math.random() * answer.length)
-            answer.splice(random, 0, correctAnswer)
-
-            return {
-              question : decode(result.question),
-              options: answer,
-              correctOption: correctAnswer
+       const delay = 1000;
+      setTimeout(() => {
+        fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
+          .then(res => {
+             if (!res.ok) {
+              throw new Error("Network response was not ok");
             }
+            return res.json()
           })
-          setQuestion(questions)
-        })
-        // If response is 404
-        .catch(err => console.log("No result found"))
+          .then(data => {
+            let results = data.results
+
+            let questions = results.map(result => {
+              let answer = result.incorrect_answers
+              let correctAnswer = result.correct_answer
+              let random = Math.ceil(Math.random() * answer.length)
+              answer.splice(random, 0, correctAnswer)
+
+              return {
+                question : decode(result.question),
+                // options: decode(answer),
+                options: answer.map(decode),
+                correctOption: decode(correctAnswer)
+              }
+            })
+            setQuestion(questions)
+            setApiError(null);
+          })
+          // If response is 404
+          .catch(err => {
+            console.error("Error fetching data:", err);
+            setApiError("Error fetching data. Please refresh the page.");
+          })
+      }, delay);
     }
   }, [isStartQuiz])
 
@@ -50,11 +76,37 @@ export default function App() {
     score = {setScore}
     checkScore = {setCheckScore}
     index = {index}
+    setShowAnswer = {setShowAnswer}
+    showAnswer = {showAnswer} 
+    setTotalScore = {setTotalScore}
     />
   )
 
       // <div className="blub-yellow"></div>
       // <div className="blub-blue"></div>
+  const resetGame = () => {
+    setQuestion([]);
+    setScore(tempScore);
+    setCheckScore(() => temp);
+    setStartQuiz(false);
+    setShowAnswer(false);
+    setTotalScore(0);
+  };
+const scoreText = <div className="scoreComp">
+    <p className="scoreEl">You scored {totalScore}/5 correct answers</p>
+    <button onClick={resetGame}>Play again</button>
+  </div>
+
+  // const playButton = !showAnswer ? 
+  //   <button onClick={()=>checkScore(score)}>Check answer</button>
+  //   : scoreText 
+
+    const playButton = !showAnswer ?
+    <button onClick={() => checkScore(score)}>Check answer</button>
+    : apiError ? (
+      <div className="error-message">{apiError}</div>
+    ) : scoreText;
+
   return (
   <div className="app-container">
       {!isStartQuiz && <Main start={setStartQuiz}/>}
@@ -63,7 +115,7 @@ export default function App() {
         
         <div className="quiz-container">
           {quizes}
-          <button onClick={()=>checkScore()}>Check answer</button>
+          {playButton}
         </div>
       }
   </div>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { decode } from 'html-entities'
 
 export default function Quiz(props) {
     const [selectedOption, setSelectedOption] = useState(null)
@@ -10,24 +9,44 @@ export default function Quiz(props) {
         const isCorrect = (option === props.correct) && temp
         props.score(prevScore => {
             let currentScore = prevScore
-            currentScore[props.index] = isCorrect
+            currentScore[props.index].isCorrect = isCorrect
+            currentScore[props.index].answer = props.correct
+            currentScore[props.index].selected = temp ? option : "none"
             return [...currentScore]
         })
 
         props.checkScore(() => checkAnswer)
     }
 
-    function checkAnswer() {
-        console.log("I am working")
+    function checkAnswer(score) {
+        let totalScoreArray = score.map(score => {
+            return score.isCorrect ? 1 : 0
+        }) 
+
+        let totalScore = totalScoreArray.reduce((acc, current)=> acc + current,0)
+
+        let answered = score.map(score=> {
+            return score.answer === "temp" || score.selected === "none"  
+                ? 0 : 1
+        })
+
+        let totalAnswered = answered.reduce((acc, current) => acc + current,0)
+
+        props.setTotalScore(totalScore)
+        totalAnswered === 5 ? props.setShowAnswer(true) : props.setShowAnswer(false)
+
     }
 
     const option = props.options.map((option, index) => 
         <p 
             key={index}
             onClick={()=> handleOption(index, option)}
-            className={`${index=== selectedOption? "selected": ""} answers-text`}
+            className={`${index=== selectedOption? "selected": ""} answers-text 
+            ${props.showAnswer && index=== selectedOption ? "red" : ""}
+            ${props.showAnswer && option === props.correct ? "green" : ""} 
+            `}
         >
-            {decode(option)}
+            {option}
         </p>
     )
     return(
